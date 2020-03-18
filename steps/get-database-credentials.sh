85fbaf40838261b297f5503943f62c99b1d673e3
Prasad Hastantram <prasad.hastantram@hmcts.net>
2020-03-18 16:25:26 +0000

    Updated Database Credentials for AAT

--- steps/get-database-credentials.sh
+++ steps/get-database-credentials.sh
@@ -18,6 +18,19 @@ case "$ENV" in
         DATA_STORE_USER="ccd"
         DATA_STORE_PASS="ccd"
         ;;
+    aattest)
+        HTTP_PROXY="proxyout.reform.hmcts.net:8080"
+        DEFINITION_STORE_HOST="localhost"
+        DEFINITION_STORE_PORT="5433"
+        DEFINITION_STORE_NAME="ccd_definition_store"
+        DEFINITION_STORE_USER="ccd@ccd-definition-store-data-migration"
+        DEFINITION_STORE_PASS='\6T8(D${;5T/Dxy)(\'
+        DATA_STORE_HOST="localhost"
+        DATA_STORE_PORT="5433"
+        DATA_STORE_NAME="ccd_data_store"
+        DATA_STORE_USER="ccd@ccd-data-store-data-migration"
+        DATA_STORE_PASS='\=MyZ{4u$(zm%5.:!\'
+        ;;
     aat)
         DEFINITION_STORE_HOST=$(az keyvault secret show)
         DEFINITION_STORE_PORT=$(az keyvault secret show)
