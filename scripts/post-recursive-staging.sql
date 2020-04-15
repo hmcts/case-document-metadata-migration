4ac47d44d3b7f557806b8c4af67b6cff702e27ce
Prasad Hastantram <prasad.hastantram@hmcts.net>
2020-04-15 13:25:04 +0100

    Scripts for Local and AAT Environment - Recursive Logic - Problem Records-Clean UP -15APR2020

--- scripts/post-recursive-staging.sql
+++ scripts/post-recursive-staging.sql
@@ -1,6 +1,36 @@
 BEGIN;
-\COPY recursive_staging FROM 'tmp/recursive-staging.csv' DELIMITER ',' CSV HEADER;
 
-\COPY recursive_staging TO 'tmp/recursive-staging.csv' DELIMITER ',' CSV HEADER;
+create index all_events_idx on all_events (case_id, document_id, event_timestamp);
+
+-- The problem cases are wherever we have a document which is present in one
+-- event for a case, absent in a later event for the same case, then present
+-- again in a yet later event for the same case.
+insert into problem_cases (jurisdiction,case_type_id,case_id,document_id,event_timestamp_1,event_timestamp_2,event_timestamp_3)
+select
+              cd.jurisdiction as jurisdiction,
+              cd.case_type_id as case_type_id,
+              e1.case_id as case_id,
+              e1.document_id as document_id,
+              e1.event_timestamp as event_timestamp_1,
+              e2.event_timestamp as event_timestamp_2,
+              e3.event_timestamp as event_timestamp_3
+from all_events e1,
+                all_events e2,
+              all_events e3,
+              case_data cd
+where e1.case_id = e2.case_id
+and   e1.document_id = e2.document_id
+and   e1.event_timestamp < e2.event_timestamp
+and   e1.doc_present
+and   not e2.doc_present
+and   e2.case_id = e3.case_id
+and   e2.document_id = e3.document_id
+and   e2.event_timestamp < e3.event_timestamp
+and   e3.doc_present
+and   cd.id = e1.case_id;
+
+\COPY all_events TO 'tmp/allevents.csv' DELIMITER ',' CSV HEADER;
+
+\COPY problem_cases TO 'tmp/problemcases.csv' DELIMITER ',' CSV HEADER;
 
 COMMIT;
--- scripts/pre-recursive-staging.sql
+++ scripts/pre-recursive-staging.sql
@@ -1,13 +1,27 @@
 BEGIN;
 
-drop table if exists recursive_staging cascade;
-CREATE TABLE recursive_staging (
-    id SERIAL PRIMARY KEY,
-    case_id BIGINT,
-    case_type_id VARCHAR,
-    jurisdiction VARCHAR,
-    document_id VARCHAR,
-    event_timestamp TIMESTAMP
+drop table if exists all_events cascade;
+create table all_events(
+      id SERIAL PRIMARY KEY,
+      jurisdiction VARCHAR,
+      case_type_id VARCHAR,
+      case_id BIGINT,
+      document_id VARCHAR,
+      document_url VARCHAR,
+      event_timestamp TIMESTAMP,
+      doc_present BOOLEAN
 );
 
+drop table if exists problem_cases cascade;
+create table problem_cases (
+      id SERIAL PRIMARY KEY,
+      jurisdiction VARCHAR,
+      case_type_id VARCHAR,
+      case_id BIGINT,
+      document_id VARCHAR,
+      document_url VARCHAR,
+      event_timestamp_1 TIMESTAMP,
+      event_timestamp_2 TIMESTAMP,
+      event_timestamp_3 TIMESTAMP
+);
 COMMIT;
