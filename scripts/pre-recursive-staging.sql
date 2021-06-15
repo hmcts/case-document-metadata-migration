7fefddeca705cd334ce0767417ec8db15dad9a91
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-06-15 15:44:22 +0100

    inserting into final doc_export table at the end of all recursive batches.

--- scripts/pre-recursive-staging.sql
+++ scripts/pre-recursive-staging.sql
@@ -19,6 +19,18 @@ begin
 end;
 $$ language plpgsql;
 
+drop table if exists doc_events cascade;
+create table doc_events(
+      jurisdiction VARCHAR,
+      case_type_id VARCHAR,
+      case_reference BIGINT,
+      case_id BIGINT,
+      event_timestamp TIMESTAMP,
+      k VARCHAR,
+      document_url VARCHAR,
+      document_id VARCHAR
+);
+
 drop table if exists all_events cascade;
 create table all_events(
       jurisdiction VARCHAR,
@@ -27,7 +39,6 @@ create table all_events(
       case_id BIGINT,
       case_event_id BIGINT,
       document_id VARCHAR,
-      document_url VARCHAR,
       event_timestamp TIMESTAMP,
       doc_present BOOLEAN
 );
@@ -38,7 +49,6 @@ create table doc_store_export(
       case_type_id VARCHAR,
       case_reference BIGINT,
       document_id VARCHAR,
-      document_url VARCHAR,
       event_timestamp TIMESTAMP
 );
 
--- scripts/recursive-staging.sql
+++ scripts/recursive-staging.sql
@@ -3,8 +3,7 @@ BEGIN;
 -- Recursive view extracts metadata and key/value pairs throughout the JSON tree,
 -- then filters for just the document_binary_url keys, stripping
 -- the URLs to leave just the document ID (not very clever regexes here).
-drop table if exists doc_events cascade;
-create table doc_events as
+insert into doc_events
 with recursive foo as
 (
               select jurisdiction, case_type_id, case_reference, case_id, event_timestamp, (entry).key as k, (entry).value as v
@@ -24,43 +23,5 @@ with recursive foo as
 select jurisdiction, case_type_id, case_reference, case_id, event_timestamp, k, replace(regexp_replace(replace(v::text,'/binary',''),'.*/',''),'"','') as document_id, v::text as document_url
 from foo
 where k = 'document_binary_url';
-
-create index doc_events_idx on doc_events (case_id, document_id, event_timestamp);
-
-
--- Every possible combination of document and event for the same case, with an
--- indicator of whether the document exists on the case JSON for the event.
--- If a case has 5 unique documents and 10 events, we should get 50 rows.
--- Each row will have a doc_present column indicating whether the given document
--- was in the JSON for the given event.
-insert into all_events(jurisdiction,case_type_id,case_reference,case_id,case_event_id,document_id,document_url,event_timestamp,doc_present)
-              select
-			  cd.jurisdiction as jurisdiction,
-			  cd.case_type_id as case_type_id,
-              cd.case_reference as case_reference,
-              cd.case_id as case_id,
-              ce.id as case_event_id,
-              cd.document_id as document_id,
-              cd.document_url as document_url,
-              ce.created_date as event_timestamp,
-              (exists (select 1 from doc_events de
-              where de.case_id = cd.case_id
-              and de.document_id = cd.document_id
-              and de.event_timestamp = ce.created_date)) as doc_present
-from case_event ce,
-              (select distinct jurisdiction,case_type_id,case_reference,case_id,document_id,document_url
-                from doc_events) cd
-                where ce.case_data_id = cd.case_id;
-
-insert into doc_store_export(jurisdiction,case_type_id,case_reference,document_id,document_url,event_timestamp)
-        select ae1.jurisdiction,ae1.case_type_id,ae1.case_reference,ae1.document_id,ae1.document_url,ae1.event_timestamp
-        from doc_events ae1
-        inner join
-        (
-          SELECT document_id, max(event_timestamp) as mts
-          FROM doc_events
-          GROUP BY document_id
-        ) ae2 on ae2.document_id = ae1.document_id and ae1.event_timestamp = ae2.mts
-        ORDER BY case_id,document_id, event_timestamp;
 
 COMMIT;
