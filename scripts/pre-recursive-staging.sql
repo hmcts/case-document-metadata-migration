bbfa278d05e7a36a622655f094828c916d5d24c4
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-06-23 16:47:05 +0100

    added population of all_events inside of each batch.

--- scripts/pre-recursive-staging.sql
+++ scripts/pre-recursive-staging.sql
@@ -7,8 +7,19 @@ create table doc_events(
       case_reference BIGINT,
       case_id BIGINT,
       event_timestamp TIMESTAMP,
+      document_id VARCHAR
+);
+
+drop table if exists all_events cascade;
+create table all_events(
+      jurisdiction VARCHAR,
+      case_type_id VARCHAR,
+      case_reference BIGINT,
+      case_id BIGINT,
+      case_event_id BIGINT,
       document_id VARCHAR,
-      document_url VARCHAR
+      event_timestamp TIMESTAMP,
+      doc_present BOOLEAN
 );
 
 COMMIT;
