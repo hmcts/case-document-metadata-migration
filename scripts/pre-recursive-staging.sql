787025d55d1f106b5b257f9ac8a890f325e803a7
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-06-23 18:06:51 +0100

    Revert "added population of all_events inside of each batch."
    This reverts commit bbfa278d05e7a36a622655f094828c916d5d24c4.

--- scripts/pre-recursive-staging.sql
+++ scripts/pre-recursive-staging.sql
@@ -7,19 +7,8 @@ create table doc_events(
       case_reference BIGINT,
       case_id BIGINT,
       event_timestamp TIMESTAMP,
-      document_id VARCHAR
-);
-
-drop table if exists all_events cascade;
-create table all_events(
-      jurisdiction VARCHAR,
-      case_type_id VARCHAR,
-      case_reference BIGINT,
-      case_id BIGINT,
-      case_event_id BIGINT,
       document_id VARCHAR,
-      event_timestamp TIMESTAMP,
-      doc_present BOOLEAN
+      document_url VARCHAR
 );
 
 COMMIT;
